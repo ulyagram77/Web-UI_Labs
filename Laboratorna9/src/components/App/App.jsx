219ba0d5f6632828c36cc5741eb0a3e5ef956cc0
whitegolyb <kirillulianow@gmail.com>
2023-11-25 23:43:29 +0200

    added task 2 not done

--- Laboratorna9/src/components/App/App.jsx
+++ Laboratorna9/src/components/App/App.jsx
@@ -1,6 +1,8 @@
-import MyCounter from '../MyCounter/MyCounter';
-import CounterList from '../CounterList/CounterList';
-import { ConfigProvider, Divider } from 'antd';
+import { useState } from 'react';
+import MyCounter from '../Counter/MyCounter/MyCounter';
+import CounterList from '../Counter/CounterList/CounterList';
+import Cart from '../Cart/Cart';
+import { ConfigProvider, Divider, Row, Col, Slider, Typography } from 'antd';
 import './App.css';
 
 const theme = {
@@ -19,7 +21,43 @@ const counters = [
     { id: 3 },
 ];
 
+const products = [
+    {
+        id: 1,
+        name: 'Asus TUF Gaming',
+        price: 1000,
+        quantity: 1,
+        total: 50,
+        image: '/products/tuf.png',
+    },
+    {
+        id: 2,
+        name: 'Macbook Pro 13',
+        price: 2000,
+        quantity: 1,
+        total: 300,
+        image: '/products/macb.png',
+    },
+    {
+        id: 3,
+        name: 'Lenovo ThinkPad',
+        price: 700,
+        quantity: 3,
+        total: 500,
+        image: '/products/thnkp.png',
+    },
+    {
+        id: 4,
+        name: 'Asus ROG',
+        price: 1500,
+        quantity: 2,
+        total: 227,
+        image: '/products/rog.png',
+    },
+];
+
 function App() {
+    const [rows, setRows] = useState(2);
     return (
         <>
             <ConfigProvider theme={theme}>
@@ -34,6 +72,26 @@ function App() {
                 <Divider>
                     <b>TASK 2</b>
                 </Divider>
+                <Row>
+                    <Col
+                        xs={24}
+                        md={{ span: 16, offset: 4 }}
+                    >
+                        <Typography.Title level={4}>
+                            Products Cart
+                        </Typography.Title>
+                        <Slider
+                            min={1}
+                            max={6}
+                            defaultValue={rows}
+                            onChange={setRows}
+                        />
+                        <Cart
+                            rows={rows}
+                            products={products}
+                        />
+                    </Col>
+                </Row>
             </ConfigProvider>
         </>
     );
--- Laboratorna9/src/components/Cart/Cart.jsx
+++ Laboratorna9/src/components/Cart/Cart.jsx
@@ -1,5 +1,57 @@
-const Cart = () => {
-    return <div></div>;
+import { Table, Typography, Image } from 'antd';
+
+const columns = [
+    {
+        title: 'Name',
+        dataIndex: 'name',
+        key: 'name',
+        render: (text) => <Typography.Text copyable>{text}</Typography.Text>,
+    },
+    {
+        title: 'Price',
+        dataIndex: 'price',
+        key: 'price',
+        sorter: (a, b) => a.price - b.price,
+    },
+    {
+        title: 'Quantity',
+        dataIndex: 'quantity',
+        key: 'quantity',
+    },
+    {
+        title: 'Total',
+        dataIndex: 'total',
+        key: 'total',
+    },
+    {
+        title: 'Image',
+        dataIndex: 'image',
+        key: 'image',
+        render: (text) => (
+            <Image
+                src={text}
+                width={100}
+                alt="product"
+            />
+        ),
+    },
+];
+
+const Cart = ({ products, rows }) => {
+    const cartData = products.map((product) => ({
+        ...product,
+        key: product.id,
+    }));
+
+    return (
+        <Table
+            dataSource={cartData}
+            columns={columns}
+            pagination={{
+                pageSize: rows,
+            }}
+        />
+    );
 };
 
 export default Cart;
